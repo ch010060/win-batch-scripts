3c1e4a6e5fec0b3daa72f63afb2dc22712a2d05d
ch010060 <ch010060@gmail.com>
2020-08-27 17:05:12 +0800

    Add network tool

---
+++ NetworkTool/Add default route.bat
@@ -0,0 +1,3 @@
+TITLE "Add default route"
+echo "Add default routing gateway"
+route ADD 0.0.0.0 mask 0.0.0.0 [IP] METRIC 55
---
+++ NetworkTool/Remove defualt route.bat
@@ -0,0 +1,3 @@
+TITLE "Remove defualt route"
+echo "Delete default routing gateway"
+route delete 0.0.0.0 mask 0.0.0.0 [IP] -p
