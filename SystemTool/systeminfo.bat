e88d53d46e4ddc6a610ceb640a1bd3832259698e
ch010060 <ch010060@gmail.com>
2020-08-27 16:38:58 +0800

    Add system related tool

---
+++ SystemTool/systeminfo.bat
@@ -0,0 +1,30 @@
+    : FULL SYSTEM INFO    
+    : START CMD    
+
+    : Set the dialog title    
+    title FULL SYSTEM INFO    
+
+    @echo off    
+
+    : Display nothing but add whitespace    
+     echo.
+
+    : Displaying text    
+    echo Exporting System Info...
+
+    : Using the SYSTEMINFO command and export it directly into a text file    
+    SYSTEMINFO>INFO.txt
+
+    : Display nothing but add whitespace    
+    echo.
+
+    : Displaying text    
+    echo INFO.txt Exported!
+
+    : Open the newly exported file in notepad    
+    notepad.exe INFO.txt    
+
+    : Wait on user    
+    pause    
+
+    : STOP CMD
