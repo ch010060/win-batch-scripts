d4d1641d5e929528091e8fb619abbf76d020d118
ch010060 <ch010060@gmail.com>
2023-03-26 17:23:42 +0800

    Add Calibre pre-processing scripts related to rar5.

---
+++ Calibre/convert_rar5_to_rar4.ps1
@@ -0,0 +1,24 @@
+﻿$folder_SRC = "<SRC_PATH>"
+$folder_PROCESS = "<PROCESS_PATH>"
+$folder_COMPLETED = "<COMPLETED_PATH>"
+	
+$files = (Get-ChildItem -Path "${folder_SRC}\" -Filter "*.rar" -Recurse -File| Select-Object FullName)
+foreach ($file in $files){
+            $file_src=$file.FullName
+			$file_PROCESS = $file_src.replace("]","``]").replace("[","``[")
+            $file_BASENAME=(Get-Item $file_PROCESS ).Basename
+            $file_EXTENSION=(Get-Item $file_PROCESS ).Extension
+            $file_DIR=(Get-Item $file_PROCESS ).DirectoryName
+            $filename="${file_BASENAME}${file_EXTENSION}"
+
+            # Check is rar5 ?
+            $list=(Rar.exe l "$file_src")
+            $match=(echo $list | Select-String "...D...")
+            if($match){ # shortcut to skip this folder
+                    echo "Process: $file_src"
+                    Rar.exe x "$file_src" "$folder_PROCESS"
+                    Rar.exe a -rr3 -ma4 -r -ep1 "$folder_COMPLETED\$filename" "$folder_PROCESS\*"
+                    Remove-Item -path "$folder_PROCESS" -Force -Recurse
+                    mkdir "$folder_PROCESS"
+            }
+}
---
+++ Calibre/copy_rar5_to_dst.ps1
@@ -0,0 +1,17 @@
+﻿$folder_SRC = "<SRC_PATH>"
+$folder_DST = "<DST_PATH>"
+	
+$files = (Get-ChildItem -Path "${folder_SRC}\" -Filter "*.rar" -Recurse -File| Select-Object FullName)
+foreach ($file in $files){
+            $file_src=$file.FullName
+			$file_CHECK = $file_src.replace("]","``]").replace("[","``[")
+
+            # Check is rar5 ?
+            $list=(Rar.exe l "$file_src")
+            $match=(echo $list | Select-String "RAR 5")
+            if($match){ # shortcut to skip this folder
+                    echo "Process: $file_src"
+                    Copy-Item "$file_CHECK" -Destination "$folder_DST"
+            }
+
+}
